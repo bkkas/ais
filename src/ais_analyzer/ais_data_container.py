f98bae8ef235927f22dcc9c4e1c11ee0cad3a7be
Tord Nytun <tord.nytun@student.uib.no>
2022-10-03 16:23:33 +0200

    add(ais_data_container.py) add wrapper functions and corresponding tests

--- src/ais_analyzer/ais_data_container.py
+++ src/ais_analyzer/ais_data_container.py
@@ -3,21 +3,26 @@ import numpy as np
 
 
 class AisDataContainer:
-    """ Container / wrapper class for our ais data in pandas DataFrames """
+    """ Container class for our ais data in pandas DataFrames """
+
     def __init__(self, data=None):
         self.data = data
 
-    def initialize_data(self, input_path):
-        """ Function to pass data to the container """
-        self.data = pd.read_csv(input_path, sep=';')
+    def read_csv(self, *args, **kwargs):
+        """ Function to pass data to the container given a path to a csv """
+        self.data = pd.read_csv(*args, **kwargs)
+
+    def append(self, data_to_append):
+        data_to_append = pd.DataFrame(data_to_append)
+        self.data.append(data_to_append)
 
     def get_representation(self):
         """ Output the container object type """
         print("AIS data representation:", type(self.data))
 
-    def print_head(self):
+    def head(self, *args, **kwargs):
         """ Print the first rows of the AIS data """
-        print(self.data.head())
+        print(self.data.head(*args,**kwargs))
 
-    def get_nr_rows(self):
-        return np.shape(self.data)[0]
+    def shape(self):
+        return self.data.shape
--- tests/test_ais_data_container.py
+++ tests/test_ais_data_container.py
@@ -5,11 +5,29 @@ import src.ais_analyzer.ais_data_container as adc
 class MyTestCase(unittest.TestCase):
 
 
-
-    def test_create_container(self):
+    def test_read_csv(self):
         nordata = adc.AisDataContainer()
-        nordata.initialize_data('tests/data/ais_test_data_10_rader_mongstad.csv')
-        self.assertEqual(nordata.get_nr_rows(), 10)
+        nordata.read_csv('tests/data/ais_test_data_10_rader_mongstad.csv', sep=';')
+        self.assertEqual(nordata.shape()[0], 10)
+
+    def test_shape(self):
+        # TODO
+        pass
+
+
+    def test_append(self):
+        # TODO
+        pass
+
+
+    def test_get_representation(self):
+        # TODO
+        pass
+
+
+    def test_head(self):
+        #TODO
+        pass
 
 
 if __name__ == '__main__':
