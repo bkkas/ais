6e12cb137482cb8631f4ab81b514e0c24c680bf3
saraahmadi3 <saraahmadi3@hotmail.com>
2022-10-05 23:15:51 +0200

    feat(method:check_csv_sep) Added method in input_handler.py to check the seperator in the csv file to support both no and dk files

--- src/ais_analyzer/handlers/input_handler.py
+++ src/ais_analyzer/handlers/input_handler.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 import pandas as pd
 
 
@@ -18,6 +19,16 @@ class InputHandler:
         #  colreduce,
         #  downsampling,
         #  downcasting
+        sep = self.check_csv_sep(path,sep)
 
         df = pd.read_csv(path, sep=sep)
         return df
+
+    def check_csv_sep(self, path,sep) -> str:
+        if sep!=',':
+            return sep
+        df= pd.read_csv(path, sep=sep, nrows=1)
+        nr_col = df.shape[1]
+        if nr_col==1:   #norwegian data
+            sep=';'
+        return sep
