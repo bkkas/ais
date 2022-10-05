d5dac85ae9ade8d32b2240cbba4fda8b4a157102
Sondren1288 <sondrenordmoen@hotmail.com>
2022-10-05 18:11:30 +0200

    test(tests/) Began outlining of tests
    Began outlining tests to ensure functionality of program

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,5 +1,31 @@
+import pytest
+import pandas as pd
+from src.ais_analyzer.commands import statistics as st
 
 
-class TestCase:
-    def test_something(self):
-        assert True  # add assertion here
+# Read danish dataset
+@pytest.fixture
+def read_dk() -> pd.DataFrame:
+    path = "tests/data/dk_test_data_10k_rows.csv"
+    yield pd.read_csv(path)
+
+
+# Read norwegian dataset
+@pytest.fixture
+def read_no() -> pd.DataFrame:
+    path = "tests/data/no_test_data_vestland_10k_rows.csv"
+    yield pd.read_csv(path, sep=';')
+
+
+class TestCaseNorwegian:
+
+    # Test that the full statistics method is equivalent with .describe
+    def test_statistics_no(self, read_no):
+        assert st.statistics(read_no, {'full': True}).equals(read_no.describe())
+
+
+class TestCaseDanish:
+
+    # Test that the full statistics method is equivalent with .describe
+    def test_something(self, read_dk):
+        assert st.statistics(read_dk, {'full': True}).equals(read_dk.describe())
