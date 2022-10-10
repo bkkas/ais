e34cf6af57724629cb8e8cf8620c04076b9ec9c4
saraahmadi3 <saraahmadi3@hotmail.com>
2022-10-10 18:32:37 +0200

    style(formatting:input_handler.py) changed formatting based on linter

--- src/ais_analyzer/handlers/input_handler.py
+++ src/ais_analyzer/handlers/input_handler.py
@@ -1,42 +1,44 @@
 import pandas as pd
 import values
 
+
+def check_csv_sep(path, sep) -> str:
+    if sep != ',':
+        return sep
+    df = pd.read_csv(path, sep=sep, nrows=1)
+    nr_col = df.shape[1]
+    if nr_col == 1:  # norwegian data
+        sep = ';'
+    return sep
+
+
 class InputHandler:
 
     def __init__(self, path=None, value=None):
         self.path = path
-        self.value=values()
+        self.value = values()
 
     def read_from_csv(self, path, sep=",") -> pd.DataFrame:
         """
-        :param sep: the seperator
+        :param sep: the separator, string
+        :param path: the path to the csv file, string
         :return: a dataframe object
         """
-
 
         # TODO's:
         #  support for danish and norwegian datasets,
         #  colreduce,
         #  downsampling,
         #  downcasting
-        sep = self.check_csv_sep(path,sep)
+        sep = check_csv_sep(path, sep)
         columns = self.get_needed_cols(sep)
 
         df = pd.read_csv(path, sep=sep, usecols=columns)
         return df
 
-    def check_csv_sep(self, path,sep) -> str:
-        if sep!=',':
-            return sep
-        df= pd.read_csv(path, sep=sep, nrows=1)
-        nr_col = df.shape[1]
-        if nr_col==1:   #norwegian data
-            sep=';'
-        return sep
+    def get_needed_cols(self, sep) -> list:
 
-    def get_needed_cols(self,sep) -> list:
-
-        if sep==';':
+        if sep == ';':
             return self.value.get_no_cols()
-        if sep==',':
+        if sep == ',':
             return self.value.get_dk_cols()
