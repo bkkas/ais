805a587aeb417bbd6a103701da769e98450e6f8d
Sondren1288 <sondrenordmoen@hotmail.com>
2022-10-17 02:26:21 +0200

    feat(package version) add automated patch bump
    When pushing to unittest-branch (currently), bump the minor version
    number.

---
+++ .github/bump.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Get current version
+PREV_VER="$(<version.txt)"
+echo "Previous version:"
+echo $PREV_VER
+
+# Split the version into an array of 
+# Major, minor, patch
+# read -a splits the arguments into an array
+read -r -a versions <<< $(echo $PREV_VER | tr '.' ' ')
+
+# Increment patch version
+patch_ver=$(( ${versions[2]} + 1))
+
+# Get the new version
+NEW_VER="${versions[0]}.${versions[1]}.${patch_ver}"
+echo "New version:"
+echo $NEW_VER
+echo $NEW_VER > version.txt
+
+
