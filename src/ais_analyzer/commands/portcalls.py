e33cb020b17f58b241d11306640026894a03ab27
Tord Nytun <tord.nytun@student.uib.no>
2022-10-23 17:05:52 +0200

    feat(portcalls) add function to filter transiting vessels, threshold 2kts

--- src/ais_analyzer/commands/portcalls.py
+++ src/ais_analyzer/commands/portcalls.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 import geopy
 import geopy.distance as gpd
+import datetime as dt
 
 
 def vessels_in_radius(df: pd.DataFrame, point: tuple, radius: float) -> pd.DataFrame:
-
     # 1. Create a latlon tuple-like column
     # TODO remove if not useful later
     df['latlon'] = list(zip(df.lat, df.lon))
@@ -52,12 +52,33 @@ def vessels_in_radius(df: pd.DataFrame, point: tuple, radius: float) -> pd.DataF
     # 2. Calculate the distance to the center point
     # 3. Keep row if distance is less than radius
 
-
-    #within_r = lambda p1: (geopy.distance.distance(point, p1).km)/1000 < radius
+    # within_r = lambda p1: (geopy.distance.distance(point, p1).km)/1000 < radius
 
     # Create a mask of bools for rows that are within_r
 
     return df
+
+
+def validate_input_parameters(args):
+    _required_list = ['lat', 'lon', 'radius']
+    _args_present = [args[arg] for arg in _required_list]
+
+    if None in _args_present:
+        _missing = [arg for arg, present in zip(_required_list, _args_present) if not present]
+        raise KeyError(f"Missing arguments for portcalls: {_missing}")
+
+
+def remove_transiting_vessels(vessels: pd.DataFrame) -> pd.DataFrame:
+    # First strategy: filter on speed (knots)
+    # We set the threshold to account for drift (if ships are on ancher or similarly)
+    speed_threshold = 2
+    vessels_no_transits = vessels.loc[vessels['sog'] < speed_threshold]
+
+    # Second strategy: filter on time
+    # If vessel spend less than x time in area, remove all instances
+    # But for now include all, independent of time in area
+
+    return vessels_no_transits
 
 
 def portcalls(input_df: pd.DataFrame, args: dict) -> pd.DataFrame:
@@ -67,21 +88,11 @@ def portcalls(input_df: pd.DataFrame, args: dict) -> pd.DataFrame:
 
     """
     # Input validation
-    _required_list = ['lat', 'lon', 'radius']
-    _args_present = [args[arg] for arg in _required_list]
-
-    if None in _args_present:
-        _missing = [arg for arg, present in zip(_required_list, _args_present) if not present]
-        raise KeyError(f"Missing arguments for portcalls: {_missing}")
-
+    validate_input_parameters(args)
 
     # Step 1: Filter on ships that are in the radius
-    # - Create inRadius function, apply to df and create boolean mask
-    # - Apply mask to the df
-
     center_coord = (args['lat'], args['lon'])
     radius = args['radius']
-
     vessels = vessels_in_radius(input_df, center_coord, radius)
 
     # Step 2: Filter on the vessels that are idle at some point
@@ -89,4 +100,12 @@ def portcalls(input_df: pd.DataFrame, args: dict) -> pd.DataFrame:
     # - Check if geo position stays within a certain area over certain amount of time?
     # -
 
-    return input_df
+
+    # Remove vessels that are only transiting area
+    vessels = remove_transiting_vessels(vessels)
+
+    # Add two columns to df: arrive and depart
+    # This is the first and last rows of a grouping on MMSI
+
+
+    return vessels
