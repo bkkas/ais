47c6eca81403f951f1d1855bc006520ae699baa2
saraahmadi3 <saraahmadi3@hotmail.com>
2022-10-24 13:30:08 +0200

    feat(method::simple_down_sampler) adds a method in input_handler.py that downsamples. removes every 30 second apperance of a vessel.

--- src/ais_analyzer/handlers/input_handler.py
+++ src/ais_analyzer/handlers/input_handler.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from values import *
+from .values import *
 
 
 def check_csv_sep(path, sep) -> str:
@@ -24,6 +24,16 @@ def get_needed_dtypes(sep) -> dict:
         return get_no_dtypes()
     if sep == ',':
         return get_dk_cols()
+
+
+def simple_down_sampler(df) -> pd.DataFrame:
+    groups = df.groupby(df.columns[1])
+    dfs = []
+    for name, group_df in groups:
+        reduced_df = group_df[~group_df['# Timestamp'].dt.floor('30s').duplicated()]
+        dfs.append(reduced_df)
+    reduced_df = pd.concat(dfs).sort_index()
+    return reduced_df
 
 
 class InputHandler:
@@ -55,6 +65,8 @@ class InputHandler:
         dts = get_needed_cols(sep)
         self.dtypes = dts
 
-        df = pd.read_csv(path, sep=sep, usecols=columns)
+        df = pd.read_csv(path, sep=sep, usecols=columns, dtype=dts, parse_dates=columns[0])
+        df = simple_down_sampler(df)
         self.df = df
         return df
+
