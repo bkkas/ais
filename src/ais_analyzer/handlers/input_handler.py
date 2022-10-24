46c677c45b3b291435405d9a1f684ef4f605fcbd
Sondren1288 <sondrenordmoen@hotmail.com>
2022-10-24 16:06:32 +0200

    Refactor(input_handler) Functions moved to class

--- src/ais_analyzer/handlers/input_handler.py
+++ src/ais_analyzer/handlers/input_handler.py
@@ -2,40 +2,6 @@ import pandas as pd
 from .values import *
 
 
-def check_csv_sep(path, sep) -> str:
-    if sep != ',':
-        return sep
-    df = pd.read_csv(path, sep=sep, nrows=1)
-    nr_col = df.shape[1]
-    if nr_col == 1:  # norwegian data
-        sep = ';'
-    return sep
-
-
-def get_needed_cols(sep) -> list:
-    if sep == ';':
-        return get_no_cols()
-    if sep == ',':
-        return get_dk_cols()
-
-
-def get_needed_dtypes(sep) -> dict:
-    if sep == ';':
-        return get_no_dtypes()
-    if sep == ',':
-        return get_dk_dtypes()
-
-
-def simple_down_sampler(df) -> pd.DataFrame:
-    groups = df.groupby(df.columns[1])
-    dfs = []
-    for name, group_df in groups:
-        reduced_df = group_df[~group_df['# Timestamp'].dt.floor('30s').duplicated()]
-        dfs.append(reduced_df)
-    reduced_df = pd.concat(dfs).sort_index()
-    return reduced_df
-
-
 class InputHandler:
 
     def __init__(self, path=None, sep=None, columns=None, df=None, dtypes=None):
@@ -44,6 +10,39 @@ class InputHandler:
         self.sep = sep
         self.columns = columns
         self.df = df
+
+    def check_csv_sep(self) -> None:
+        sep = self.sep
+        path = self.path
+        if sep != ',':
+            self.sep = sep
+            return
+        df = pd.read_csv(self.path, sep=self.sep, nrows=1)
+        nr_col = df.shape[1]
+        if nr_col == 1:  # norwegian data
+            self.sep = ';'
+        return
+
+    def get_needed_cols(self) -> None:
+        if self.sep == ';':
+            self.columns = get_no_cols()
+        if self.sep == ',':
+            self.columns = get_dk_cols()
+
+    def get_needed_dtypes(self) -> None:
+        if self.sep == ';':
+            self.dtypes = get_no_dtypes()
+        if self.sep == ',':
+            self.dtypes = get_dk_dtypes()
+
+    def simple_down_sampler(self) -> None:
+        groups = self.df.groupby(self.df.columns[1])
+        dfs = []
+        for name, group_df in groups:
+            reduced_df = group_df[~group_df['# Timestamp'].dt.floor('30s').duplicated()]
+            dfs.append(reduced_df)
+        reduced_df = pd.concat(dfs).sort_index()
+        self.df = reduced_df
 
     def read_from_csv(self, path, sep=",") -> pd.DataFrame:
         """
@@ -57,16 +56,18 @@ class InputHandler:
         #  colreduce, OK
         #  downsampling,
         #  downcasting, OK
+        if not self.sep:
+            self.sep = sep
         self.path = path
-        sep = check_csv_sep(path, sep)
-        self.sep = sep
-        columns = get_needed_cols(sep)
-        self.columns = columns
-        dts = get_needed_dtypes(sep)
-        self.dtypes = dts
+        self.check_csv_sep()
+        self.get_needed_cols()
+        self.get_needed_dtypes()
 
-        df = pd.read_csv(path, sep=sep, usecols=columns, dtype=dts, parse_dates=[columns[0]])
-        df = simple_down_sampler(df)
-        self.df = df
-        return df
+        self.df = pd.read_csv(self.path,
+                              sep=self.sep,
+                              usecols=self.columns,
+                              dtype=self.dtypes,
+                              parse_dates=[self.columns[0]])
+        self.simple_down_sampler()
+        return self.df
 
