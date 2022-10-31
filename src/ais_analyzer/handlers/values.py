e5b61a8c7285a114842b546bce526ad2b650c969
Tord Nytun <tord.nytun@student.uib.no>
2022-10-31 09:39:13 +0100

    feat(values.py) add standardized column names and corresponing lookup substrings

--- src/ais_analyzer/handlers/values.py
+++ src/ais_analyzer/handlers/values.py
@@ -3,7 +3,16 @@ def get_no_cols() -> list:
 
 
 def get_dk_cols() -> list:
-    return ['# Timestamp', 'MMSI', 'Navigational status', 'Latitude', 'Longitude', 'SOG', 'Length']
+    return ['# Timestamp', 'MMSI', 'Latitude', 'Longitude', 'Navigational status', 'SOG', 'COG', 'IMO', 'Callsign',
+            'Name', 'Ship type', 'Cargo type', 'Width', 'Length', 'Draught', 'Destination']
+
+
+def get_standardized_cols() -> dict:
+    lookup_cols = ['time', 'mmsi', 'imo', 'lat', 'lon', 'nav', 'dest', 'lenght', 'draught', 'width', 'sog', 'ship_type',
+                   'cargo_type', 'name', 'callsign']
+    std_cols = ['timestamp_utc', 'mmsi', 'imo', 'lat', 'lon', 'nav_status', 'dest', 'length', 'draught', 'width', 'sog',
+                'ship_type', 'cargo_type', 'name', 'callsign']
+    return dict(zip(lookup_cols, std_cols))
 
 
 def get_no_dtypes() -> dict:
