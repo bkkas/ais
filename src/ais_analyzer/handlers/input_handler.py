241d19c5ff8e6b7e74c7a1d3cf3de3071e9fb162
Tord Nytun <tord.nytun@student.uib.no>
2022-10-31 09:40:28 +0100

    refactor(input_handler::_infer_csv) remove most instance variables and change function names to reflect public & private funcs

--- src/ais_analyzer/handlers/input_handler.py
+++ src/ais_analyzer/handlers/input_handler.py
@@ -4,71 +4,76 @@ from .values import *
 
 class InputHandler:
 
-    def __init__(self, path=None, sep=None, columns=None, df=None, dtypes=None):
-        self.dtypes = dtypes
+    def __init__(self, path=None):
         self.path = path
-        self.sep = sep
-        self.columns = columns
-        self.df = df
+        self.data = self._read_from_csv()
 
-    def check_csv_sep(self) -> None:
-        sep = self.sep
-        path = self.path
-        if sep != ',':
-            self.sep = sep
-            return
-        df = pd.read_csv(self.path, sep=self.sep, nrows=1)
-        nr_col = df.shape[1]
-        if nr_col == 1:  # norwegian data
-            self.sep = ';'
-        return
+    def _infer_csv_sep(self, path) -> str:
+        print(f"path is {path}")
+        reader = pd.read_csv(path, sep=None, iterator=True, engine='python')
+        sep = reader._engine.data.dialect.delimiter
+        print(f"path is {path}, sep is {sep}")
+        return sep
 
-    def get_needed_cols(self) -> None:
-        if self.sep == ';':
-            self.columns = get_no_cols()
-        if self.sep == ',':
-            self.columns = get_dk_cols()
+    def _get_needed_cols(self, sep) -> list:
+        if sep == ';':
+            return get_no_cols()
+        if sep == ',':
+            return get_dk_cols()
 
-    def get_needed_dtypes(self) -> None:
-        if self.sep == ';':
-            self.dtypes = get_no_dtypes()
-        if self.sep == ',':
-            self.dtypes = get_dk_dtypes()
+    def _standardize_cols(self, df) -> pd.DataFrame:
+        col_std = get_standardized_cols()
+        columns = list(df.columns)
 
-    def simple_down_sampler(self) -> None:
-        groups = self.df.groupby(self.df.columns[1])
+        # replacing with standards
+        for i, col in enumerate(columns):
+            for key, value in col_std.items():
+                if key in col:
+                    columns[i] = value
+
+        df.columns = columns
+
+        return df
+
+    def _get_needed_dtypes(self, sep) -> dict:
+        if sep == ';':
+            return get_no_dtypes()
+        if sep == ',':
+            return get_dk_dtypes()
+
+    def _simple_down_sampler(self, df) -> None:
+        groups = df.groupby(df.columns[1])
         dfs = []
+        print(f"Cols {df.columns}")
         for name, group_df in groups:
-            reduced_df = group_df[~group_df[self.columns[0]].dt.floor('30s').duplicated()]
+            reduced_df = group_df[~group_df['timestamp_utc'].dt.floor('30s').duplicated()]
             dfs.append(reduced_df)
         reduced_df = pd.concat(dfs).sort_index()
-        self.df = reduced_df
+        return reduced_df
 
-    def read_from_csv(self, path, sep=",") -> pd.DataFrame:
-        """
-        :param sep: the separator, string
-        :param path: the path to the csv file, string
-        :return: a dataframe object
-        """
+    def _read_from_csv(self) -> pd.DataFrame:
 
         # TODO:
         #  standardize column names
-        #  down sampling, make better
-        #       - chunking
+        #  down sampling, make better strategy
         #       - remove all boat above/below certain values
+        #       - ?
+        #  chunking
 
-        if not self.sep:
-            self.sep = sep
-        self.path = path
-        self.check_csv_sep()
-        self.get_needed_cols()
-        self.get_needed_dtypes()
+        sep = self._infer_csv_sep(self.path)
+        columns = self._get_needed_cols(sep=sep)
+        dtypes = self._get_needed_dtypes(sep=sep)
 
-        self.df = pd.read_csv(self.path,
-                              sep=self.sep,
-                              usecols=self.columns,
-                              dtype=self.dtypes,
-                              parse_dates=[self.columns[0]])
-        self.simple_down_sampler()
-        return self.df
+        input_df = pd.read_csv(self.path,
+                               sep=sep,
+                               usecols=columns,
+                               dtype=dtypes,
+                               parse_dates=[columns[0]])
 
+        input_df = self._standardize_cols(input_df)
+
+        self._simple_down_sampler(input_df)
+        return input_df
+
+    def get_data(self):
+        return self.data
