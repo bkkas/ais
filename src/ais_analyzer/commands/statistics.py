53c034b9d75e906781ba81d2c701631fc91cf51d
kevin.dale <vuq015@uib.no>
2022-11-02 19:28:13 +0100

    feat(statistic.py) adds a method to see the countries from mmsi and number of rows from countries have -add

--- src/ais_analyzer/commands/statistics.py
+++ src/ais_analyzer/commands/statistics.py
@@ -13,6 +13,18 @@ def statistics(input_df: pd.DataFrame, args) -> pd.DataFrame:
     if args['full']:
         stat_df = input_df.describe(include='all')
         return stat_df
+    elif args['country']:
+        mmsi = pd.read_excel("statistics_data/mmsikos.xlsx")
+        digits = pd.DataFrame({'numbs': input_df["mmsi"].astype("str").str[:-6].astype("int")})
+
+        counts = pd.DataFrame(data = digits.value_counts()).reset_index()
+        counts.columns = ["Digit", "Count"]
+        mmsi_count = pd.merge(mmsi, counts, on='Digit')
+
+        nr_of_rows_by_countries = mmsi_count.groupby(mmsi_count['Allocated to']).aggregate(sum).reset_index().drop(columns = "Digit")
+        return nr_of_rows_by_countries
+     elif args['']:
+
     else:
         stat_df = input_df.describe()
         return stat_df
