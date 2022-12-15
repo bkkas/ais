2b9a55a177d70c75f0f601db5a4c571e535c171c
kevin.dale <vuq015@uib.no>
2022-12-15 15:08:57 +0100

    completed statistics tests

--- src/ais_analyzer/commands/statistics.py
+++ src/ais_analyzer/commands/statistics.py
@@ -17,6 +17,7 @@ def statistics(input_df: pd.DataFrame, args) -> pd.DataFrame:
         # Outputs each country represented from mmsi, and how many rows the dataset includes from that
         # mmsi = pd.read_excel("statistics_data/mmsikos1.xlsx")
         mmsi = pd.DataFrame(mmsi_map.items(), columns=["Digit", "Allocated to"])
+        mmsi = mmsi.rename(columns={'Allocated to' : 'country'})
 
         unique_ais = pd.DataFrame({"nr_unique_ships" : input_df["mmsi"].unique()})
         unique_ais["Digit"] = unique_ais["nr_unique_ships"].astype("str").str[:-6].astype("int")
@@ -25,23 +26,25 @@ def statistics(input_df: pd.DataFrame, args) -> pd.DataFrame:
         ais_counts = pd.DataFrame(data = ais_digits.value_counts(), columns = ["nr_rows"]).reset_index()
 
         # del ais
-
-        ships_country = pd.merge(mmsi, unique_ais, on='Digit')
+        try:
+            ships_country = pd.merge(mmsi, unique_ais, on='Digit')
+        except:
+            print("mmsi does not point to country")
 
         del unique_ais
         del ais_digits
 
-        nr_of_ships_by_country = pd.DataFrame(data = ships_country.groupby('Allocated to')['nr_unique_ships'].nunique().reset_index())
+        nr_of_ships_by_country = pd.DataFrame(data = ships_country.groupby('country')['nr_unique_ships'].nunique().reset_index())
 
         del ships_country
 
         mmsi_count = ais_counts.merge(mmsi, on='Digit')
-        nr_of_rows_by_countries = mmsi_count.groupby(mmsi_count['Allocated to']).aggregate(sum).reset_index().drop(columns = "Digit")
+        nr_of_rows_by_countries = mmsi_count.groupby(mmsi_count['country']).aggregate(sum).reset_index().drop(columns = "Digit")
 
         del mmsi
         del mmsi_count
 
-        data = nr_of_ships_by_country.merge(nr_of_rows_by_countries, on="Allocated to")
+        data = nr_of_ships_by_country.merge(nr_of_rows_by_countries, on="country")
 
         del nr_of_ships_by_country
         del nr_of_rows_by_countries
@@ -52,16 +55,16 @@ def statistics(input_df: pd.DataFrame, args) -> pd.DataFrame:
         occurence = input_df.sort_values(by=["timestamp_utc"]).loc[: ,["mmsi", "timestamp_utc"]]
 
         first_oc = occurence.groupby(by=["mmsi"]).first().reset_index()
-        first_oc["last"] = occurence.groupby(by=["mmsi"]).last().reset_index().loc[:,"timestamp_utc"]
+        first_oc["last_oc"] = occurence.groupby(by=["mmsi"]).last().reset_index().loc[:,"timestamp_utc"]
 
         del occurence
 
 
-        first_oc = first_oc.rename(columns={"timestamp_utc" : "first"})
+        first_oc = first_oc.rename(columns={"timestamp_utc" : "first_oc"})
 
         df_lenght = pd.DataFrame(input_df.loc[: ,["mmsi","length"]].groupby(by=["mmsi"]).first().reset_index())
         count_rows = input_df["mmsi"].value_counts().reset_index()
-        count_rows.columns = ["mmsi", "count"]
+        count_rows.columns = ["mmsi", "count_rows"]
         df_lenght = df_lenght.merge(count_rows, on="mmsi")
 
         del count_rows
@@ -76,11 +79,13 @@ def statistics(input_df: pd.DataFrame, args) -> pd.DataFrame:
     elif args['complete']:
         # outputs one row with information about the whole dataset
         one_line = pd.DataFrame(input_df["mmsi"].unique()).count()
-        one_line = pd.DataFrame(one_line, columns=["count"])
+        one_line = pd.DataFrame(one_line, columns=["mmsi_count"])
 
-        one_line["avg_lenght"] = input_df.length.unique().mean()
-        one_line["max_lenght"] = input_df.length.unique().max()
-        one_line["min_lenght"] = input_df.length.unique().min()
+        unique_ais = input_df.groupby(by=["mmsi"]).first().reset_index()
+
+        one_line["avg_length"] = unique_ais.length.mean()
+        one_line["max_length"] = unique_ais.length.max()
+        one_line["min_length"] = unique_ais.length.min()
 
         one_line["start_date"] = input_df.timestamp_utc.sort_values().min()
         one_line["end_date"] = input_df.timestamp_utc.sort_values().max()
--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -3,19 +3,116 @@ import pytest
 from src.ais_analyzer.commands import statistics
 
 @pytest.fixture
+def args_country():
+    yield {
+        'full': False,
+        'mmsi': False,
+        'country': True,
+        'complete': False,
+    }
+
+@pytest.fixture
+def args_mmsi():
+    yield {
+        'full': False,
+        'mmsi': True,
+        'country': False,
+        'complete': False,
+    }
+
+@pytest.fixture
+def args_complete():
+    yield {
+        'full': False,
+        'mmsi': False,
+        'country': False,
+        'complete': True,
+    }
+
+@pytest.fixture
 def country_arg():
-    return 'country'
+    yield {'args' : 'country'}
 
 @pytest.fixture
 def single_mmsi():
     df = pd.read_csv("tests/data/std_single_portcall.csv")
     yield df
 
+@pytest.fixture
+def stat_mmsi():
+    df = pd.read_csv("tests/data/std_statistics_test.csv")
+    yield df
+
 class TestCase:
-    def test_single_mmsi(self, single_mmsi, country_arg):
+    def test_single_mmsi_country(self, single_mmsi, args_country):
         df = single_mmsi
-        country_mmsi = statistics.statistics(single_mmsi, country_arg)
-        assert len(df.rows) == 1
+        country_mmsi = statistics.statistics(df, args_country)
+        assert len(country_mmsi) == 1
         assert country_mmsi.country[0] == "Denmark"
         assert country_mmsi.nr_unique_ships[0] == 1
-        assert country_mmsi.nr_rows[0] == 45
+        assert country_mmsi.nr_rows[0] == 44
+
+    def test_single_mmsi_mmsi(self, single_mmsi, args_mmsi):
+        df = single_mmsi
+        mmsi_mmsi = statistics.statistics(df, args_mmsi)
+        assert len(mmsi_mmsi) == 1
+        assert mmsi_mmsi.mmsi[0] == 219016713
+        assert str(mmsi_mmsi.first_oc[0]) == "2022-08-05 10:00:00"
+        assert str(mmsi_mmsi.last_oc[0]) == "2022-08-05 14:05:00"
+        assert mmsi_mmsi.length[0] == 100
+        assert mmsi_mmsi.count_rows[0] == 44
+
+    def test_single_mmsi_complete(self, single_mmsi, args_complete):
+        df = single_mmsi
+        mmsi_complete = statistics.statistics(df, args_complete)
+        assert len(mmsi_complete) == 1
+        assert mmsi_complete.avg_length[0] == 100
+        assert mmsi_complete.max_length[0] == 100
+        assert mmsi_complete.min_length[0] == 100
+        assert str(mmsi_complete.start_date[0]) == "2022-08-05 10:00:00"
+        assert str(mmsi_complete.end_date[0]) == "2022-08-05 14:05:00"
+        assert mmsi_complete.median_lon[0] == 42
+        assert mmsi_complete.median_lat[0] == 42
+
+    def test_stat_mmsi_country(self, stat_mmsi, args_country):
+        df = stat_mmsi
+        country_mmsi = statistics.statistics(df, args_country)
+        assert len(country_mmsi) == 5
+        countries = ["Denmark", "France", "Ireland", "Malta", "Spain"]
+        country_mmsi.country[0] == "Denmark"
+        print(country_mmsi.head())
+        for i in range(len(countries)):
+            assert country_mmsi.country[i] == countries[i]
+        assert country_mmsi.nr_unique_ships[0] == 2
+        assert country_mmsi.nr_unique_ships[1] == 3
+        assert country_mmsi.nr_unique_ships[2] == 1
+        assert country_mmsi.nr_rows[0] == 24
+        assert country_mmsi.nr_rows[1] == 3
+        assert country_mmsi.nr_rows[2] == 70
+    
+    def test_single_mmsi_mmsi(self, stat_mmsi, args_mmsi):
+        df = stat_mmsi
+        mmsi_mmsi = statistics.statistics(df, args_mmsi)
+        assert len(mmsi_mmsi) == 9
+        assert mmsi_mmsi.mmsi[0] == 219016713
+        assert str(mmsi_mmsi.first_oc[0]) == "2022-08-05 06:00:00"
+        assert str(mmsi_mmsi.last_oc[0]) == "2022-08-06 05:00:00"
+        assert str(mmsi_mmsi.first_oc[2]) == "2022-08-05 11:00:00"
+        assert str(mmsi_mmsi.last_oc[2]) == "2022-08-05 11:00:00"
+        assert mmsi_mmsi.length[0] == 100
+        assert mmsi_mmsi.length[2] == 105
+        assert mmsi_mmsi.count_rows[0] == 20
+        assert mmsi_mmsi.count_rows[8] == 70
+
+    def test_single_mmsi_complete(self, stat_mmsi, args_complete):
+        df = stat_mmsi
+        mmsi_complete = statistics.statistics(df, args_complete)
+        assert len(mmsi_complete) == 1
+        assert mmsi_complete.mmsi_count[0] == 9
+        assert mmsi_complete.avg_length[0] == 117.33333333333333
+        assert mmsi_complete.max_length[0] == 210
+        assert mmsi_complete.min_length[0] == 100
+        assert str(mmsi_complete.start_date[0]) == "2022-08-05 06:00:00"
+        assert str(mmsi_complete.end_date[0]) == "2022-08-06 05:00:00"
+        assert mmsi_complete.median_lat[0] == 50.5
+        assert mmsi_complete.median_lon[0] == 50.5
